937b3346a71aedae05eacb0b6e503ea88c272608
ssabrut <michael.gunawan2002@gmail.com>
2022-05-30 21:13:39 +0700

    Created splash screen, login and register view

---
+++ keeb-wiki/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  keeb-wiki
+//
+//  Created by MacBook Pro on 30/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            Text("Content View")
+                .foregroundColor(.white)
+                .font(.system(size: 30))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
